91208445357633b92d65e832e7cb945b143c0931
patrice02 <patricedev02@gmail.com>
2023-03-16 19:33:58 +0000

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * print:str:"Programming is like building a multilingual puzzle
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
