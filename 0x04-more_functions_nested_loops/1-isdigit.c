0fb5d0f3d9db465671067a25f426062c46a14d87
patrice02 <patricedev02@gmail.com>
2023-03-24 09:04:17 +0000

    add function to check if the input i a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+
+/**
+  *_isdigit - check if the input is digit
+  *Return: 1 if true and 0 if not
+  *@c: the current nput arg
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
