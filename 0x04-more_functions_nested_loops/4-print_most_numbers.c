7ffca4c1acbb3f983cf51204f4910f479ff129b4
patrice02 <patricedev02@gmail.com>
2023-03-24 11:07:16 +0000

    add function to print number 01356789

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+int _putchar(char);
+
+/**
+ * print_most_numbers - print number from 0 to 9
+ * without 2 and 4
+ * Return:None
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (!(i == 50) && !(i == 52))
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
