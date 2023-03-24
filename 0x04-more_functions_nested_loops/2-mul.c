5a2babf59bfd905089cf10069afeba508bd23fa8
patrice02 <patricedev02@gmail.com>
2023-03-24 10:49:16 +0000

    function to print 0 to 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies two integers.
+ *@a: first value.
+ *@b: second value.
+ * Return: the multpied value.
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+int _putchar(char);
+
+/**
+ * print_numbers - print number between 0 to 9
+ */
+
+
+void print_numbers(void)
+{	
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
