ee4d6ee35b5e42cdb5c089ea786c265be03d01f5
patrice02 <patricedev02@gmail.com>
2023-03-24 13:48:40 +0000

    printing triangle of \

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,27 @@ void print_diagonal(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
-	{
-		int j;
-
-		for (j = 0; j < n; j++)
-		{
-			if (j != i)
-			{
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\\');
-			}
-		}
+	if (n <= 0)
 		_putchar('\n');
+	else
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; j < n; j++)
+			{
+				if (j < i)
+				{
+					_putchar(' ');
+				}
+				else if (j == i)
+				{
+					_putchar('\\');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
