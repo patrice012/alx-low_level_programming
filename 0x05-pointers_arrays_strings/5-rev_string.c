f593197d23a747ac31d5c9a1edf98f289d60658e
patrice02 <patricedev02@gmail.com>
2023-03-27 19:11:36 +0000

    resolte task 7-9

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,11 +18,11 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (i = 0; i < len / 2; i++) 
+	for (i = 0; i < len / 2; i++)
 	{
 		swap = s[i];
 		s[i] = s[len - i - 1];
 		s[len - i - 1] = swap;
-    	}
+	}
 }
 
