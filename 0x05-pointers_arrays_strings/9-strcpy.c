e5b9036247b016936ba68fb37556de4aaedf5f63
patrice02 <patricedev02@gmail.com>
2023-03-30 22:27:02 +0000

    update project

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -20,14 +20,10 @@ char *_strcpy(char *dest, char *src)
 		len++;
 	}
 
-	i = 0;
-
-	while (i < len)
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
-
 	dest[i] = '\0';
 
 	return (dest);
