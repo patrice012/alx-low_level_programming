1c4ec9672878eacc2a41866249501f5b8023e643
patrice02 <patricedev02@gmail.com>
2023-04-05 14:25:25 +0000

    update reverse recursion function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,14 +9,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_print_rev_recursion((s + 1));
-		_putchar(*s);
-	}
-	else
-	{
-		_putchar('\n');
+	if (!*s)
 		return;
-	}
+	_print_rev_recursion((s + 1));
+	_putchar(*s);
 }
