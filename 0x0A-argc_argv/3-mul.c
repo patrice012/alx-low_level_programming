f41ddda2f95d12cc7efb601ab2216f2b25955ead
patrice02 <patricedev02@gmail.com>
2023-04-10 18:54:55 +0000

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
 {
 	if (argc < 3 || argc > 3)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (1);
 	}
 	else
-		printf("%d\n", atoi(argv[argc - 2]) * atoi(argv[argc - 1]));
-	return (EXIT_SUCCESS);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (EXIT_SUCCESS);
 
 }
 
