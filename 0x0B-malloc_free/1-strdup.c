24fccb6f04278ef0d21c568e0f1c94e557bfd22f
patrice02 <patricedev02@gmail.com>
2023-04-11 18:19:56 +0000

    add task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "main.h"
 
+int _len(char *str);
+
+
 /**
  * *_strdup - copies the string given as parameter
  * @str: string to duplicate
@@ -11,8 +14,40 @@
 
 char *_strdup(char *str)
 {
+	char *dup;
+	unsigned int len, i;
 
 	if (str == NULL)
 		return (NULL);
 
+	len = _len(str);
+
+	dup = malloc(sizeof(char) * (len + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i];
+	}
+	dup[i] = '\0';
+
+	return (dup);
+}
+
+
+
+
+/**
+  * _len - get the length of parameter
+  * @str: string
+  * Return: length
+  */
+
+int _len(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	return (1 + _len(str + 1));
 }
