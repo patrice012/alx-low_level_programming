3195eed06615fbab3d7f0d5450689933aec482e7
patrice02 <patricedev02@gmail.com>
2023-04-11 18:27:52 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+  * _len - get the length of parameter
+  * @str: string
+  * Return: length
+  */
+
+int _len(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	return (1 + _len(str + 1));
+}
+
+
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	len1 = _len(s1);
+	len2 = _len(s2);
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
+
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
+}
+
