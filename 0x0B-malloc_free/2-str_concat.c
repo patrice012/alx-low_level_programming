5b0bda7a29ef1d6b5f74d73acbaaabf11b0246eb
patrice02 <patricedev02@gmail.com>
2023-04-11 20:00:03 +0000

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,15 +29,15 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	len1 = _len(s1);
-	len2 = _len(s2);
-
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	len1 = _len(s1);
+	len2 = _len(s2);
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (s3 == NULL)
 		return (NULL);
