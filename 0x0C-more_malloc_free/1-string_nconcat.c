da3d7cf8a593ac6f77bc41a1f04a7540bd7b2565
patrice02 <patricedev02@gmail.com>
2023-04-13 18:57:24 +0000

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,17 +15,22 @@ int _len(char *str)
 	return (1 + _len(str + 1));
 }
 /**
-* string_nconcat - function that concatenates two strings.
-* @s1: first string parameter
-* @s2: second string parameter
-* @n:number of s2 bytes to copy
-* Return: pointer to new concatenate string
-*/
+  * string_nconcat - function that concatenates two strings.
+  * @s1: first string parameter
+  * @s2: second string parameter
+  * @n:number of s2 bytes to copy
+  * Return: pointer to new concatenate string
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1, len2, len3, i, j;
 	char *s3;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	/* get length */
 	len1 = _len(s1);
@@ -38,15 +43,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len3 = len1 + len2 + 1;
 
 	s3 = malloc(sizeof(char) * len3);
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
+
+	if (s3 == NULL)
+		return (0);
+
 	/* copy s1 */
 	for (i = 0; i < len1; i++)
 	{
 		*(s3 + i) = *(s1 + i);
 	}
+
+
 	/* copy s2 */
 	for (j = 0; j < len3 - len1; j++)
 	{
