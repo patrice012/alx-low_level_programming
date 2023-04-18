e32c23d367db4879212b5aaa00c7738c3f8173bb
patrice02 <patricedev02@gmail.com>
2023-04-18 10:27:35 +0000

    fixe typedef define error

--- 0x0E-structures_typedef/3-main.c
+++ 0x0E-structures_typedef/3-main.c
@@ -2,14 +2,12 @@
 #include "dog.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+  * main - The entry point of the program
+  * Return: int .
  */
 int main(void)
 {
 	dog_t my_dog;
-
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
 	my_dog.owner = "Bob";
