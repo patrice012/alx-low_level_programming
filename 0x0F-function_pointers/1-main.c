b46c76d3ec280319783dac2aec27528b2b542d7c
patrice02 <patricedev02@gmail.com>
2023-04-23 10:53:06 +0000

    add task 2

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -2,34 +2,32 @@
 #include "function_pointers.h"
 
 /**
-  * print_elem - the function name
-  * @elem: parameter of type int .
-  * Return: void .
- */
+* print_elem - the function name
+* @elem: parameter of type int .
+* Return: void .
+*/
 void print_elem(int elem)
 {
 	printf("%d\n", elem);
 }
-
 /**
-  * print_elem_hex - the function name
-  * @elem: parameter of type int .
-  * Return: void .
- */
+* print_elem_hex - the function name
+* @elem:
+* Return:
+*/
 void print_elem_hex(int elem)
 {
 	printf("0x%x\n", elem);
 }
-
 /**
-  * main - The entry point of the program
-  * Return: int .
- */
+* main - The entry point of the program
+* Return:
+*/
 int main(void)
 {
 	int array[5] = {0, 98, 402, 1024, 4096};
 
-	array_iterator(array, 5, &print_elem);
-	array_iterator(array, 5, &print_elem_hex);
+	array_iterator (array, 5, &print_elem);
+	array_iterator (array, 5, &print_elem_hex);
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,55 +1,51 @@
 #include "3-calc.h"
 
 /**
-  * op_add - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
- */
+* op_add - the function name
+* @a: parameter of type int .
+* @b: parameter of type int .
+* Return: int .
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
-  * op_sub - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
- */
+* op_sub - the function name
+* @a:
+* @b:
+* Return:
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
-  * op_mul - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
- */
+* op_mul - the function name
+* @a:
+* @b:
+* Return:
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
-  * op_div - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
- */
+* op_div - the function name
+* @a:
+* @b:
+* Return:
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
-  * op_mod - the function name
-  * @a: parameter of type int .
-  * @b: parameter of type int .
-  * Return: int .
- */
+* op_mod - the function name
+* @a:
+* @b:
+* Return:
+*/
 int op_mod(int a, int b)
 {
 	return (a % b);
