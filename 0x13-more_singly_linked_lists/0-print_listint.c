2e2ae4cf5e550ba32863d972cbc63fd0d82be5e4
patrice02 <patricedev02@gmail.com>
2023-04-29 20:20:48 +0000

    add task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stddef.h>
+#include "lists.h"
+
+int _putchar(char c);
+
+/**
+  * print_listint -  function that prints all the elements of a listint_t list.
+  * @h: linked list
+  * Return: the size of the list
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	if (!h)
+		return (-1);
+
+	while (h)
+	{
+		_putchar('0' + h->n);
+		_putchar('\n');
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,8 +1,6 @@
 #ifndef _LISTS_H
 #define _LISTS_H
 
-size_t print_listint(const listint_t *h);
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,4 +13,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
 #endif
