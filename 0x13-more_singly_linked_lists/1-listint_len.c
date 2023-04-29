d27720dec74b47c002887c9a439324bc67adcc26
patrice02 <patricedev02@gmail.com>
2023-04-29 20:30:47 +0000

    add task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include "lists.h"
+
+/**
+  * listint_len - get the length of the list
+  * @h: the list data type object
+  * Return: the size of the list
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	if (!h)
+		return (-1);
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
