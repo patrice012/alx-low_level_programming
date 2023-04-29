e7e1426dd3fae52855f4c620d37cd39228987882
patrice02 <patricedev02@gmail.com>
2023-04-29 19:53:09 +0000

    create 0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+size_t print_listint(const listint_t *h);
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+#endif
