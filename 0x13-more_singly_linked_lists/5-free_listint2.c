e43b6286474929ae5b9b6960bfe9c54f8b17fe10
patrice02 <patricedev02@gmail.com>
2023-04-30 22:10:54 +0000

    add task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (!head)
+		return;
+
 	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
-		*head = temp;
+		(*head) = temp;
 	}
-	free(head);
+
+	*head = NULL;
 }
