1f3956695514f22be47352f187d9ebbe2ca7afb6
patrice02 <patricedev02@gmail.com>
2023-05-02 15:50:50 +0000

    add task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+  * print_listint_safe -  function that prints a listint_t linked list.
+  * @head: pointer to struct
+  * Return: Nothing
+  */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *temp = head, *temp2 = NULL;
+	size_t i;
+
+	if (!temp)
+		return (-1);
+
+	i = 0;
+	while (temp != NULL)
+	{
+		printf("[%p] %d\n", (void *)(temp), (temp)->n);
+		i++;
+		temp2 = temp;
+		temp = temp->next;
+
+		if (temp2 <= temp)
+		{
+			printf("-> [%p] %d\n", (void *) temp, temp->n);
+			exit (98);
+		}
+	}
+	return (i);
+}
