87ac7ddf18cfa2277e3bfd39bf40dcb7018633fa
patrice02 <patricedev02@gmail.com>
2023-05-02 15:59:11 +0000

    update task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,17 +14,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	listint_t *temp;
-
 	/* create new node */
 	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
 
 	if (!new || !*head)
 		return (NULL);
-
 	/* set new node data */
 	new->n = n;
 	new->next = NULL;
-
 	/* add at the beginning of the list if head == NULL or index == 0 */
 	if ((*head) == NULL || idx == 0)
 	{
@@ -32,16 +29,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-
 	/* get the element at the given index */
 	temp = *head;
-
 	while (i < idx - 1 && temp->next)
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	/* if i < index => add to the end of the list */
 	if (i < (idx - 1) && temp->next == NULL)
 	{
