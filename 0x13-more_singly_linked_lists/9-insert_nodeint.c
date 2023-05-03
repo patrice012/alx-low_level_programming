7e97bea16af2d25ebc0096dce63076945a12e6a1
patrice02 <patricedev02@gmail.com>
2023-05-03 20:29:22 +0000

    update task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,6 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (i < (idx - 1) && temp->next == NULL)
 	{
 		temp->next = new;
+		return (new->next);
 	}
 	else
 	{
