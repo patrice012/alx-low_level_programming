d947f6390fa07992e106715e1a188bdf3fff4529
patrice02 <patricedev02@gmail.com>
2023-05-07 21:57:09 +0000

    remove executable files

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,18 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
+	unsigned long int sum;
 	int i;
 
-	sum3 = 0;
-	sum5 = 0;
 	sum = 0;
 
 	for (i = 0; i < 1024; ++i)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			sum += i;
 		}
 	}
-	sum = sum3 + sum5;
 	printf("%lu\n", sum);
 	return (0);
 }
