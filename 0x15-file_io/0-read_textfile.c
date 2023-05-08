00c0eb11fd640ae95e76f1855a93f4510d00881a
patrice02 <patricedev02@gmail.com>
2023-05-08 22:43:49 +0000

    fixe betty error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,26 +12,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
-	ssize_t bytes_read, bytes_written;
+	ssize_t b_read, b_written;
 
 	if (!filename)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL) {
-		return 0;
+	if (buffer == NULL)
+	{
+		return (0);
 	}
 	/* open file */
 	fd = open(filename, O_RDONLY);
 	/* read file content into  buffer */
-	bytes_read = read(fd, buffer, letters);
+	b_read = read(fd, buffer, letters);
 	/* write content into STDOUT */
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	b_written = write(STDOUT_FILENO, buffer, b_read);
 	/* check the amount of bytes */
-	if (fd == - 1 || bytes_read == -1 ||bytes_written == -1 || bytes_written != bytes_read)
+	if (fd == -1 || b_read == -1 || b_written == -1 || b_written != b_read)
 	{
 		close(fd);
 		return (0);
 	}
 	close(fd);
-	return (bytes_written);
+	return (b_written);
 }
