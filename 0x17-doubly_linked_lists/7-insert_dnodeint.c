f87138cf340bcfdaed35ccdde29b4db4c011e501
patrice02 <patricedev02@gmail.com>
2023-06-15 22:11:49 +0000

    fix memory error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,37 +11,32 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	dlistint_t *temp = *h, *node;
+	dlistint_t *temp, *node;
 
 	node = malloc(sizeof(dlistint_t));
 	if (h == NULL || node == NULL)
 		return (NULL);
 	node->n = n;
-	node->next = NULL, node->prev = NULL;
+	temp = *h;
+
 	if (idx == 0)
 	{
 		node->next = *h;
+		node->prev = NULL;
 		*h = node;
 		if (node->next)
 			node->next->prev = node;
 		return (node);
 	}
+
 	while (temp)
 	{
 		if (idx == count)
 		{
-			if (temp->next == NULL)
-			{
-				temp->next = node;
-				node->prev = temp;
-			}
-			else
-			{
-				node->next = temp;
-				temp->prev->next = node;
-				node->prev = temp->prev;
-				temp->prev = node;
-			}
+			node->next = temp;
+			temp->prev->next = node;
+			node->prev = temp->prev;
+			temp->prev = node;
 			return (node);
 		}
 		temp = temp->next;
