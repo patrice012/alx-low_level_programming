575c97732e31ffb323d2679f4aa70b33d39ee311
patrice02 <patricedev02@gmail.com>
2023-06-15 13:44:20 +0000

    add task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+  * print_dlistint - print list elements
+  * @h: list head
+  * Return: number of nodes
+  */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	const dlistint_t *temp = h;
+
+	if (temp == NULL)
+		return (0);
+	while(temp)
+	{
+		count++;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+	return (count);
+}
