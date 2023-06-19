be1f193a6a1c6818fe39c91c667d3df7a8ccf88e
patrice02 <patricedev02@gmail.com>
2023-06-19 21:52:25 +0000

    add task 2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,65 @@
+/**
+ * add - add 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - remainder of division of 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
