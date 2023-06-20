380f65c4dfda0988f73b1d4337df8b536c775675
patrice02 <patricedev02@gmail.com>
2023-06-20 14:22:39 +0000

    update task 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -2,7 +2,7 @@
 
 shopt -s nullglob
 
-c_files=( $(ls *.c | grep -v "0-main.c") )
+c_files=( $(ls *.c) )
 name="liball.so"
 
 shopt -u nullglob
@@ -11,9 +11,7 @@ if [ ${#c_files[@]} -eq 0 ]; then
     echo "No .c file found"
 else
    for file in "${c_files[@]}"; do
-       if [ "$file" != "0-main.c" ]; then
            gcc -fpic -c "$file"
-       fi
    done
 fi
 
