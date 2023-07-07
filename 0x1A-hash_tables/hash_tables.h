40e6e37f443a9043b1bb0837b542b2654f7031e5
patrice02 <patricedev02@gmail.com>
2023-07-07 21:36:16 +0000

    update

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,7 +4,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -35,9 +35,20 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
-
+/* sorted hash tables */
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 #endif
