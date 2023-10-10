d98aabec4a0fb8f7af2de01a46b5e1a9f62b44ca
patrice <patricedev02@gmail.com>
2023-10-10 21:18:33 +0000

    binary seach

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,14 +14,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    int i = 0;
-    int len = (int)size;
+	int i = 0;
+	int len = (int)size;
 
-    for (i = 0; i < len; i++)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
-    return -1;
+	for (i = 0; i < len; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,77 @@
+#include <stddef.h>
+#include <stdio.h>
 #include "search_algos.h"
 
-
 /**
- * _print - helper function use to print an array
- * @arr: array to print
- */
-
-void _print(int *array)
+* _print - Helper function used to print array's element.
+* @array: The array to be printed.
+* @size: The size of the array.
+*/
+void _print(int *array, size_t size)
 {
-    int i = 0;
-    while (array[i])
-    {
-        printf("%d,", array[i]);
-        i++;
-    }
+	size_t i = 0;
+
+	printf("Searching in array: ");
+	while (i < size)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
 }
 
-
 /**
- * binary_search - function that searches for a value in an array of
- * integers using the Linear search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: value is the value to search for
- * Return:  the first index where value is located, return -1 if
- * value is not present in array or if array is NULL
- */
-
+* binary_search - Searches for a value in a sorted array
+* of integers using the Binary search algorithm.
+* @array: The sorted array to search in.
+* @size: The size of the array.
+* @value: The value to search for.
+*
+* Return: The index of the value in the array, or -1 if not found.
+*/
 int binary_search(int *array, size_t size, int value)
 {
-    int left = 0, right = (int)size - 1, m = 0;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid = left + (right - left) / 2;
+
+	if (array == NULL)
+		return (-1);
+	printArray(array, size);
+	if (mid && mid % 2 == 0)
+		mid--;
 
 
-    while (left < right)
-    {
-        m = (int)(left + right);
-        if (array[m] > value)
-            right = m;
-        else
-            left = m + 1;
-    }
-    return -1;
-    
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+
+
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] < value)
+		{
+			left = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return (-1);
+		}
+		if (left <= right)
+		{
+			printArray(&array[left], right - left + 1);
+		}
+	}
+	return (-1);
 }
